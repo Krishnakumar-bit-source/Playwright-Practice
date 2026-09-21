41d92e21a5f9dc8c7a11ecae791803f1ca2ccbee
Krishnakumar-bit-source <krishnakumars1995@gmail.com>
2026-09-21 17:11:01 +0530

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -1,9 +1,6 @@
 // @ts-check
 import { defineConfig, devices } from '@playwright/test';
 
-/**
- * @see https://playwright.dev/docs/test-configuration
- */
 export default defineConfig({
   testDir: './tests',
   timeout: 40000,
